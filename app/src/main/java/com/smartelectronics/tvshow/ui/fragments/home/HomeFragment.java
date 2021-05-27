27c38e81c77997ce7b9fe224b7b2f305b881b2c0
saeed <avrpic8@gmail.com>
2021-05-27 12:21:45 +0430

    exit dialog in exit app

--- app/src/main/java/com/smartelectronics/tvshow/ui/fragments/home/HomeFragment.java
+++ app/src/main/java/com/smartelectronics/tvshow/ui/fragments/home/HomeFragment.java
@@ -1,29 +1,27 @@
-    package com.smartelectronics.tvshow.ui.fragments.home;
+package com.smartelectronics.tvshow.ui.fragments.home;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.smartelectronics.tvshow.R;
 import com.smartelectronics.tvshow.adapter.PopularTvShowAdapter;
 import com.smartelectronics.tvshow.databinding.FragmentHomeBinding;
-
 import com.smartelectronics.tvshow.viewModels.MostPopularTvShowViewModel;
 
 
@@ -54,6 +52,14 @@ public class HomeFragment extends Fragment {
         initRecyclerView();
         initRefreshLayout();
 
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                showExitDialog();
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
+
         Log.i("cycle", "onCreateView: ");
         return binding.getRoot();
     }
@@ -69,19 +75,19 @@ public class HomeFragment extends Fragment {
         Log.i("cycle", "onDestroy: ");
     }
 
-    private void initImageViews(){
-        binding.watchListImageView.setOnClickListener(click ->{
+    private void initImageViews() {
+        binding.watchListImageView.setOnClickListener(click -> {
             NavDirections action = HomeFragmentDirections.actionHomeFragmentToWatchListFragment();
             Navigation.findNavController(binding.getRoot()).navigate(action);
         });
 
-        binding.searchImageView.setOnClickListener(click ->{
+        binding.searchImageView.setOnClickListener(click -> {
             NavDirections action = HomeFragmentDirections.actionHomeFragmentToSearchFragment();
             Navigation.findNavController(binding.getRoot()).navigate(action);
         });
     }
 
-    private void initRecyclerView(){
+    private void initRecyclerView() {
         binding.tvShowsRecyclerView.setHasFixedSize(true);
         adapter = new PopularTvShowAdapter(tvShowViewModel.tvShowItemsList, "home");
         binding.tvShowsRecyclerView.setAdapter(adapter);
@@ -89,28 +95,28 @@ public class HomeFragment extends Fragment {
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
-                if(!binding.tvShowsRecyclerView.canScrollVertically(1)){
+                if (!binding.tvShowsRecyclerView.canScrollVertically(1)) {
                     tvShowViewModel.increaseCurrentPage();
                     getMostPopularTvShows(tvShowViewModel.getCurrentPage());
                 }
             }
         });
 
-        if(tvShowViewModel.isAllowRefreshList()) {
+        if (tvShowViewModel.isAllowRefreshList()) {
             getMostPopularTvShows(tvShowViewModel.getCurrentPage());
             tvShowViewModel.setAllowRefreshList(false);
         }
     }
 
-    private void toggleLoading(){
-        if (tvShowViewModel.getCurrentPage() == 1){
-            if(binding.getIsLoading() != null && binding.getIsLoading()){
+    private void toggleLoading() {
+        if (tvShowViewModel.getCurrentPage() == 1) {
+            if (binding.getIsLoading() != null && binding.getIsLoading()) {
                 binding.setIsLoading(false);
             } else {
                 binding.setIsLoading(true);
             }
-        }else {
-            if(binding.getIsLoadingMore() != null && binding.getIsLoadingMore()){
+        } else {
+            if (binding.getIsLoadingMore() != null && binding.getIsLoadingMore()) {
                 binding.setIsLoadingMore(false);
                 binding.progressMore.hide();
             } else {
@@ -120,19 +126,34 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    private void showError(){
+    private void showError() {
         binding.imgError.setVisibility(View.VISIBLE);
         binding.txtError.setVisibility(View.VISIBLE);
         binding.tvShowsRecyclerView.setVisibility(View.INVISIBLE);
     }
 
-    private void hideError(){
+    private void hideError() {
         binding.imgError.setVisibility(View.GONE);
         binding.txtError.setVisibility(View.GONE);
         binding.tvShowsRecyclerView.setVisibility(View.VISIBLE);
     }
 
-    private void initRefreshLayout(){
+    private void showExitDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder
+                .setTitle("Exit app")
+                .setMessage(R.string.exit_message)
+                .setPositiveButton("Yes", (dialog, which) -> {
+                    dialog.cancel();
+                    getActivity().finish();
+                })
+                .setNegativeButton("No", ((dialog, which) -> {
+                    dialog.cancel();
+                }));
+        builder.show();
+    }
+
+    private void initRefreshLayout() {
         binding.refreshLayout.setColorScheme(R.color.blue,
                 R.color.green, R.color.orange, R.color.purple);
 
@@ -142,22 +163,22 @@ public class HomeFragment extends Fragment {
         });
     }
 
-    private void getMostPopularTvShows(int currentPage){
+    private void getMostPopularTvShows(int currentPage) {
         toggleLoading();
         tvShowViewModel.getMostPopularTvShows(currentPage).observe(getViewLifecycleOwner(), popularTvShow -> {
             toggleLoading();
-            if(popularTvShow != null){
+            if (popularTvShow != null) {
                 hideError();
                 binding.refreshLayout.setRefreshing(false);
                 tvShowViewModel.setTotalAvailablePages(popularTvShow.getPages());
-                Log.i("home", "getMostPopularTvShows: " +popularTvShow.getPages());
-                if(popularTvShow.getTvShows() != null){
+                Log.i("home", "getMostPopularTvShows: " + popularTvShow.getPages());
+                if (popularTvShow.getTvShows() != null) {
                     int oldCount = tvShowViewModel.tvShowItemsList.size();
                     tvShowViewModel.tvShowItemsList.addAll(popularTvShow.getTvShows());
                     adapter.notifyItemRangeInserted(oldCount, tvShowViewModel.tvShowItemsList.size());
                     adapter.notifyDataSetChanged();
                 }
-            }else {
+            } else {
                 binding.refreshLayout.setRefreshing(false);
                 showError();
             }
